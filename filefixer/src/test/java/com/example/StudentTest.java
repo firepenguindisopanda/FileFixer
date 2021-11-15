0d8c68e921732491cc8fe8d59adf30c424f94400
Nicholas <nicosmith.smith3@gmail.com>
2021-11-15 19:17:15 -0400

    Added testcases for Student class

---
+++ filefixer/src/test/java/com/example/StudentTest.java
@@ -0,0 +1,45 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class StudentTest {
+    private Student foo;
+    @BeforeEach
+    public void setup(){
+        foo = new Student("601725", "nicholasfoo", "81304376",true);
+    }
+    @Test
+    public void testGetNameOfStudent(){
+        System.out.println("getNameOfStudent");
+        String expResult = "nicholasfoo";
+        String result = foo.getname();
+        assertEquals(expResult, result);
+    }
+    @Test
+    public void testGetIDOfStudent(){
+        System.out.println("getIDOfStudent");
+        String expResult = "81304376";
+        String actual = foo.getID();
+        assertEquals(expResult, actual);
+    }
+    @Test
+    public void testGetPIDOfStudent(){
+        System.out.println("getPIDOfStudent");
+        String expResult = "601725";
+        String actual = foo.getPID();
+        assertEquals(expResult, actual);
+    }
+    @Test
+    public void testGetStudentAttendance(){
+        System.out.println("getAttendanceOfStudent");
+        
+        Boolean actual = foo.getAttendanceStatus();
+        assertTrue(actual);
+    }
+}
