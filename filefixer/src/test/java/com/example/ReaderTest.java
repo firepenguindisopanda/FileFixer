bcbf70c6476b5921fb206fac211ffae9ced862e0
Nicholas <nicosmith.smith3@gmail.com>
2021-11-15 23:04:44 -0400

    created testcases for methods in Reader class

---
+++ filefixer/src/test/java/com/example/ReaderTest.java
@@ -0,0 +1,40 @@
+package com.example;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+public class ReaderTest {
+    
+    private Reader testReader;
+    @BeforeEach
+    public void setup(){
+        testReader = new Reader();
+    }
+
+    @Test
+    public void testLoadCsvData(){
+        System.out.println("Testing Load Csv Data Function");
+        Student[] testLCDOutput;
+        String user = System.getProperty("user.home");
+        testLCDOutput = testReader.LoadCsvData(user+"/Desktop/toBeRenamed/Sample.csv");
+        assertTrue(testLCDOutput.length > 0);
+    }
+
+    @Test
+    public void testLoadDirectoryFiles(){
+        System.out.println("Testing Load Directory Files Function");
+        toRename[] testLDFOutput;
+        try {
+            testLDFOutput = testReader.LoadDirectoryFiles();
+            assertTrue(testLDFOutput.length > 0);
+        } catch (Exception e) {
+            System.out.println("Error while loading files from directory");
+            e.printStackTrace();
+        }
+        
+    }
+
+}
