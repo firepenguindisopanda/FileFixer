bd1f3b9997d340580c6cc206d74d58f061999586
6ixGhad <60444097+6ixGhad@users.noreply.github.com>
2021-11-20 00:13:49 -0400

    Delete filefixer directory

--- filefixer/src/main/java/com/example/IIterator.java
+++
@@ -1,6 +0,0 @@
-package com.example;
-
-public interface IIterator {
-    public Boolean hasNext();
-    public Object next();
-}
--- filefixer/src/main/java/com/example/Individual.java
+++
@@ -1,8 +0,0 @@
-package com.example;
-public interface Individual{
-    public String getPID();
-    public String getname();
-    public String getID();
-    public Boolean getAttendanceStatus();
-    public void setAttendance(Boolean status);
-}
