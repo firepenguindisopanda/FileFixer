f24c560e06efa918d6efe3e5e03e8def4289db36
6ixGhad <60444097+6ixGhad@users.noreply.github.com>
2021-11-20 00:11:47 -0400

    Add files via upload

---
+++ filefixer/src/main/java/com/example/Individual.java
@@ -0,0 +1,8 @@
+package com.example;
+public interface Individual{
+    public String getPID();
+    public String getname();
+    public String getID();
+    public Boolean getAttendanceStatus();
+    public void setAttendance(Boolean status);
+}
---
+++ filefixer/src/main/java/com/example/ToRename.java
@@ -0,0 +1,51 @@
+package com.example;
+public class ToRename implements Individual {
+
+    public String PID;
+    public String name;
+    public String ID;
+    private java.util.ArrayList <String> toBeRenamed;
+    public Boolean AttendanceStatus;
+
+    public ToRename (String PID, String name, String ID,Boolean AttendanceStatus){
+
+        this.PID = PID;
+        this.name = name;
+        this.ID = ID;
+        toBeRenamed = new java.util.ArrayList<String>();
+        this.AttendanceStatus = AttendanceStatus;
+
+
+    }
+
+    public String getPID() {
+        return PID;
+    }
+
+    public String getname() {
+        return name;
+    }
+
+
+    public String getID() {
+        return ID;
+    }
+
+    public java.util.ArrayList<String> getToBeRenamedList(){//Accessor for arraylist that stores the combo list.
+        return toBeRenamed;
+    }
+    
+    public void addToList(String originalfilename){//Mutator for the array that stores the combo list.
+        toBeRenamed.add(originalfilename);
+    }
+
+    public Boolean getAttendanceStatus() {
+        return AttendanceStatus;
+    }
+
+    public void setAttendance(Boolean status) {
+        AttendanceStatus = status;
+        
+    }
+    
+}
