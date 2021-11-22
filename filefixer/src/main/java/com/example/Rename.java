289d70eb8e947e4214257e5e104a7b1c5c9f0c61
Nicholas <nicosmith.smith3@gmail.com>
2021-11-22 15:07:40 -0400

    Added print statement to find error

--- filefixer/src/main/java/com/example/Rename.java
+++ filefixer/src/main/java/com/example/Rename.java
@@ -13,90 +13,87 @@ public class Rename {
         filesToRename = rename[0];
         int count = 0;
         int lengthForInvalidList = filesToRename.getToBeRenamedList().size() + 1;
+        System.out.println(lengthForInvalidList);
         invalid = new String[lengthForInvalidList];
-        // Calls three classes for three different types of renaming
-        SimpleRename.startRenamingProcess(count, filesToRename.getToBeRenamedList().size(), student, rename,
-                fcollection);
-        StandardRename.startRenamingProcess(count, filesToRename.getToBeRenamedList().size(), student, rename,
-                fcollection);
-        ComplexRename.startRenamingProcess(count, filesToRename.getToBeRenamedList().size(), student, rename,
-                fcollection);
-        count++;
+        //Calls three classes for three different types of renaming
+            SimpleRename.startRenamingProcess(count, filesToRename.getToBeRenamedList().size(), student, rename, fcollection);
+            StandardRename.startRenamingProcess(count, filesToRename.getToBeRenamedList().size(), student, rename, fcollection);
+            ComplexRename.startRenamingProcess(count, filesToRename.getToBeRenamedList().size(), student, rename, fcollection);
+
+            count++;
 
     }
+    
 
-    public static int checkValidity(Boolean valid, int numeral, String[] listOfFilesToBeRenamed, int count) {
+    public static int checkValidity(Boolean valid, int numeral,  String[] listOfFilesToBeRenamed, int count){
 
-        if (valid.equals(false) && !listOfFilesToBeRenamed[count].contains(".csv")
-                && listOfFilesToBeRenamed[count].contains(".pdf")) {
+        if (valid.equals(false) && !listOfFilesToBeRenamed[count].contains(".csv") && listOfFilesToBeRenamed[count].contains(".pdf")){  
             invalid[numeral] = "Problem submission to review: " + listOfFilesToBeRenamed[count];
             numeral++;
         }
         return numeral;
     }
 
-    public static int getLength(Student[] arr) {
+    public static int getLength(Student[] arr){
         int count = 0;
-        for (Student el : arr)
+        for(Student el : arr)
             if (el != null)
-                count++;
+            count++;
         return count;
     }
 
-    public static void generateTXT(int count, int sizeOfRenamingList, Boolean valid, int numeral,
-            String[] invalidSubmissions, Student[] student, String[] listOfFilesToBeRenamed) {
+    public static void generateTXT(int count, int sizeOfRenamingList, Boolean valid, int numeral, String[] invalidSubmissions, 
+    Student[] student, String[] listOfFilesToBeRenamed ){
         if (count == sizeOfRenamingList - 1 && valid == true) {
             System.out.println("\nFiles renamed in renamedFiles folder in filesToRename folder.");
-            System.out.println(
-                    "\nView missingSubmissions.txt in the project folder for the list of missing submissions.\n");
+            System.out.println("\nView missingSubmissions.txt in the project folder for the list of missing submissions.\n");
+        }
+    
+    // preparing the missing submissions text file
+    System.out.println("Check missingSubmissions.txt in project for missing submissions or submissions that need review.\n");
+
+    int number = 0;
+    int num = 0;
+    try {
+        PrintStream fileOut = new PrintStream(System.getProperty("user.dir") + "/missingSubmissions.txt");
+        System.setOut(fileOut);
+        int c = 0;
+        int counter = 0;
+        while (c < getLength(student)){
+            Student s1 = student[c];
+            while(counter < numeral){
+            if(invalid[counter].contains(s1.getname()) || invalid[counter].contains(s1.getID()) || invalid[counter].contains(s1.getname().toLowerCase())
+            || invalid[counter].contains(s1.getname().toUpperCase()) || invalid[counter].contains(s1.getname().replace(" ", "")) ){
+                 invalid[counter] = "";
+            }
+            counter++;
+            }
+            counter = 0;
+            c++;
         }
 
-        // preparing the missing submissions text file
-        System.out.println(
-                "Check missingSubmissions.txt in project for missing submissions or submissions that need review.\n");
-
-        int number = 0;
-        int num = 0;
-        try {
-            PrintStream fileOut = new PrintStream(System.getProperty("user.dir") + "/missingSubmissions.txt");
-            System.setOut(fileOut);
-            int c = 0;
-            int counter = 0;
-            while (c < getLength(student)) {
-                Student s1 = student[c];
-                while (counter < numeral) {
-                    if (invalid[counter].contains(s1.getname()) || invalid[counter].contains(s1.getID())
-                            || invalid[counter].contains(s1.getname().toLowerCase())
-                            || invalid[counter].contains(s1.getname().toUpperCase())
-                            || invalid[counter].contains(s1.getname().replace(" ", ""))) {
-                        invalid[counter] = "";
-                    }
-                    counter++;
-                }
-                counter = 0;
-                c++;
+        while (num < numeral) {
+            if(!invalid[num].equals("")){
+            System.out.println(invalid[num]);
             }
-
-            while (num < numeral) {
-                if (!invalid[num].equals("")) {
-                    System.out.println(invalid[num]);
-                }
-                num++;
-            }
-
-            while (number < getLength(student)) {
-                Student missingStudent = student[number];
-                if (missingStudent.getAttendanceStatus()) {
-                    System.out.println("Submission missing: " + missingStudent.getname() + " " + missingStudent.getID());
-
-                }
-                number++;
-            }
-            fileOut.close();
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
+            num++;
         }
+
+        while (number < getLength(student)) {
+            Student missingStudent = student[number];
+            if (missingStudent.getAttendanceStatus()) {
+                System.out.println("Submission missing: " + missingStudent.getname() + " " + missingStudent.getID());
+                
+
+            }
+            number++;
+        }
+        fileOut.close();
+    } catch (Exception e) {
+       
     }
+}
+
 
     public static void copyFiles(File originalFile, File newFile) {
         try {
